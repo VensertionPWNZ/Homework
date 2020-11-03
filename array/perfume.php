e368580fc1e5d2213fa21c0867df2b3070963b80
Vladimir <72560675+VensertionPWNZ@users.noreply.github.com>
2020-11-03 17:00:23 +0300

    Create perfume.php

---
+++ array/perfume.php
@@ -0,0 +1,313 @@
+<?php
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'Dior' => [
+            'name' => 'Dior',
+            'EAU' => [
+                'name' => 'EAU',
+                'price' => '150$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false' => 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['Dior']['name'];
+echo " ";
+echo $perfumArr['Perfum']['Dior']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['Dior']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['Dior']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['Dior']['EAU']['in_stock']['true'];
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'D&G' => [
+            'name' => 'D&G',
+        'EAU' => [
+            'name' => 'EAU',
+            'price' => '120$',
+            'size' => '100ml',
+            'in_stock' => [
+                'true' => 'have',
+                'false'=> 'havent',
+            ]
+    ]
+]
+]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['D&G']['name'];
+echo " ";
+echo $perfumArr['Perfum']['D&G']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['D&G']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['D&G']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['D&G']['EAU']['in_stock']['true'];
+echo '<br>';
+
+echo '<br>';
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'GIVENCHY' => [
+            'name' => 'GIVENCHY',
+            'EAU' => [
+                'name' => 'INDIGO',
+                'price' => '200$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['GIVENCHY']['name'];
+echo " ";
+echo $perfumArr['Perfum']['GIVENCHY']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['GIVENCHY']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['GIVENCHY']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['GIVENCHY']['EAU']['in_stock']['true'];
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'BLACK_AFGANO' => [
+            'name' => 'BLACK_AFGANO',
+            'EAU' => [
+                'name' => 'EAU',
+                'price' => '450$',
+                'size' => '30ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BLACK_AFGANO']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BLACK_AFGANO']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BLACK_AFGANO']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['BLACK_AFGANO']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['BLACK_AFGANO']['EAU']['in_stock']['false'];
+echo '<br>';
+
+echo '<br>';
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'SAINT_LAURENT' => [
+            'name' => 'SAINT_LAURENT',
+            'EAU' => [
+                'name' => 'EGOIST',
+                'price' => '80$',
+                'size' => '50ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['SAINT_LAURENT']['name'];
+echo " ";
+echo $perfumArr['Perfum']['SAINT_LAURENT']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['SAINT_LAURENT']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['SAINT_LAURENT']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['SAINT_LAURENT']['EAU']['in_stock']['false'];
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'BULGARI' => [
+            'name' => 'BULGARI',
+            'EAU' => [
+                'name' => 'BLACK',
+                'price' => '145$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BULGARI']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BULGARI']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BULGARI']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['BULGARI']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['BULGARI']['EAU']['in_stock']['true'];
+echo '<br>';
+
+echo '<br>';
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'BARDEROS' => [
+            'name' => 'BARDEROS',
+            'EAU' => [
+                'name' => 'EAU',
+                'price' => '60$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BARDEROS']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BARDEROS']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['BARDEROS']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['BARDEROS']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['BARDEROS']['EAU']['in_stock']['false'];
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'CHANEL' => [
+            'name' => 'CHANEL',
+            'EAU' => [
+                'name' => 'EL_HOMME',
+                'price' => '213$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['CHANEL']['name'];
+echo " ";
+echo $perfumArr['Perfum']['CHANEL']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['CHANEL']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['CHANEL']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['CHANEL']['EAU']['in_stock']['true'];
+echo '<br>';
+
+echo '<br>';
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'PLAYBOY' => [
+            'name' => 'PLAYBOY',
+            'EAU' => [
+                'name' => 'NEXT',
+                'price' => '50$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['PLAYBOY']['name'];
+echo " ";
+echo $perfumArr['Perfum']['PLAYBOY']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['PLAYBOY']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['PLAYBOY']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['PLAYBOY']['EAU']['in_stock']['false'];
+echo '<br>';
+
+$perfumArr = [
+    'Perfum' => [
+        'name' => 'Perfum',
+        'LOUIS_VUITTON' => [
+            'name' => 'LOUIS_VUITTON',
+            'EAU' => [
+                'name' => 'EL_HOMME',
+                'price' => '499$',
+                'size' => '100ml',
+                'in_stock' => [
+                    'true' => 'have',
+                    'false'=> 'havent',
+                ]
+            ]
+        ]
+    ]
+];
+echo $perfumArr['Perfum']['name'];
+echo " ";
+echo $perfumArr['Perfum']['LOUIS_VUITTON']['name'];
+echo " ";
+echo $perfumArr['Perfum']['LOUIS_VUITTON']['EAU']['name'];
+echo " ";
+echo $perfumArr['Perfum']['LOUIS_VUITTON']['EAU']['price'];
+echo " ";
+echo $perfumArr['Perfum']['LOUIS_VUITTON']['EAU']['size'];
+echo " ";
+echo $perfumArr['Perfum']['LOUIS_VUITTON']['EAU']['in_stock']['false'];
+echo '<br>';
