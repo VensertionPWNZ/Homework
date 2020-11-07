7a9cbbfe69346bd0f12fc02a8e685694afcc287e
Vladimir <72560675+VensertionPWNZ@users.noreply.github.com>
2020-11-07 17:05:01 +0300

    Update function.php

--- 04.11.20/test/function.php
+++ 04.11.20/test/function.php
@@ -1,16 +1,16 @@
 <?php
 
-function life($a, $b, $c)
+function request1($a, $b, $c)
 {
     return $a + $b + $c;
 }
 
-function life2($a, $b , $c)
+function request2($a, $b , $c)
 {
     return $a - $b - $c;
 }
 
-function life3($a, $b, $c)
+function request3($a, $b, $c)
 {
     return $a - $b + $c;
 }
