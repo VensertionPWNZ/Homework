233271db380e760493401f620dd93cf5a9136daa
Vladimir <72560675+VensertionPWNZ@users.noreply.github.com>
2020-11-07 17:01:50 +0300

    Create function.php

---
+++ 04.11.20/test/function.php
@@ -0,0 +1,16 @@
+<?php
+
+function life($a, $b, $c)
+{
+    return $a + $b + $c;
+}
+
+function life2($a, $b , $c)
+{
+    return $a - $b - $c;
+}
+
+function life3($a, $b, $c)
+{
+    return $a - $b + $c;
+}
