49b4557f23eb42c5705d66d0c3c66469677075eb
Vladimir <72560675+VensertionPWNZ@users.noreply.github.com>
2020-11-17 18:01:26 +0300

    Add files via upload

---
+++ 09.11.20/form.php
@@ -0,0 +1,11 @@
+<form action="/cms_template/blog_page.php" method="post">
+    <label>
+        <input type="text" name="string_1">
+        <input type="text" name="string_2">
+        <select name="nameFUNC">
+            <option value="one">Function1</option>
+            <option value="two">Function2</option>
+        </select>
+        <button> Confirm </button>
+    </label>
+</form>
